f27ade819c2b57e2850d1b103244859a7b65b35a
williamchangTW <williamchangTW@github.com>
2022-10-10 00:18:44 +0800

    [Compiler] first draft
    * learning items:
    > state machine overview
    > build the structure for state machine
    > uploads files
    
    Signed-off-by: williamchangTW <williamchangTW@github.com>

---
+++ NFAFragmentStack.h
@@ -0,0 +1,30 @@
+/**
+ * @file NFAFragmentStack.h
+ * @author William Chang (williamchangTW@github.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-10-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef _NFAFRAGMENTSTACK_H
+#define _NFAGRAGMENTSTACK_H
+
+#include "RegexpToNFA.h"
+
+#define MAX_STACK_LENGTH 1024
+
+typedef struct NFAFragmentStack
+{
+    NFAFragment buffer[MAX_STACK_LENGTH];
+    int top;
+}NFAFragmentStack;
+
+void InitNFAFragmentStack(NFAFragmentStack *pS);
+void PushNFAFragment(NFAFragmentStack *os, NFAFragment Elem);
+NFAFragment PopNFAFragment(NFAFragmentStack *ps);
+int NFAFragmentStackEmpty(NFAFragmentStack *ps);
+
+
+#endif _NFAFRAGMENTSTACK_H
